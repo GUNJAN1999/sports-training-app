b1ef087319004fb6577a43783f390efec3984ff4
GUNJAN1999 <gunjanbbps62@gmail.com>
2020-09-14 14:14:48 +0530

    this is basic sport tarining app

---
+++ src/com/student/springdemo/Coach.java
@@ -0,0 +1,9 @@
+package com.student.springdemo;
+
+public interface Coach {
+
+	public String getDailyWorkout();
+	
+	public String getDailyFortune();
+
+}
---
+++ src/com/student/springdemo/FortuneService.java
@@ -0,0 +1,7 @@
+package com.student.springdemo;
+
+public interface FortuneService {
+
+	public String getFortune();
+	
+}
